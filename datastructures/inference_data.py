0caaed720c957dcb6f6e646b5dd13fc9790ef1a1
Aftab Akhtar <aftabakhtarnaveed@outlook.com>
2022-05-09 17:35:39 +0500

    Add data structure for inference data

---
+++ datastructures/inference_data.py
@@ -0,0 +1,9 @@
+from typing import Optional
+from unicodedata import name
+from pydantic import BaseModel
+
+
+class InferenceData(BaseModel):
+    motion_generation: bool
+    file1: str
+    file2: Optional[float] = None
