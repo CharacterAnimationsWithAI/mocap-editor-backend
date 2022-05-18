2de2cc3a2ae789163f538215c675c98fafae4035
Aftab Akhtar <aftabakhtarnaveed@outlook.com>
2022-05-18 20:23:37 +0500

    Add motion style transfer library

---
+++ processing/motion_style_transfer/main.py
@@ -0,0 +1,13 @@
+from processing.motion_style_transfer.style_transfer.test import *
+
+class StyleTransfer:
+    def __init__(self, content_src, style_src, output_dir):
+        self.content_src = content_src
+        self.style_src = style_src
+        self.output_dir = output_dir
+
+
+    def apply_style_transfer(self):
+        args = argparse.Namespace(name=None, batch_size=None, config='config', content_src=self.content_src, style_src=self.style_src, output_dir=self.output_dir)
+        main(args)
+        
